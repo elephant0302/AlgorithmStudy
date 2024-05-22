406ad0f6f7cfdb8733f39aad311277d5b91dbbe1
LEE JUNSOO <77887022+elephant0302@users.noreply.github.com>
2024-05-22 10:35:24 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int answer = a+b;
+        System.out.print(answer);
+	}
+}
