687eabd71cedc8eb71c5c19c3f40e501ffc4470f
LEE JUNSOO <77887022+elephant0302@users.noreply.github.com>
2024-07-25 15:32:28 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,14 +1,4 @@
-s = input()
-result = [-1]*26
-cnt = 0
-for i in s:
-    n = ord(i)
-    if n>=97 and n<=122 and result[n-97]==-1:
-        result[n-97]=cnt
-        cnt+=1
-    else:
-        cnt+=1
-        
+S = input()
 
-for i in result:
-    print(i,end=" ")
+for x in 'abcdefghijklmnopqrstuvwxyz':
+    print(S.find(x), end = ' ')
