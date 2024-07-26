9f26dd0f0fd70f3998dd637d7eebee64eedca762
LEE JUNSOO <77887022+elephant0302@users.noreply.github.com>
2024-07-26 10:23:10 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,8 +1,6 @@
-s = list(input())
-n = s[::-1]
-cnt = 1
-for i in range(len(s)):
-    if s[i] != n[i]:
-        cnt -=  1
-        break
-print(cnt)
+word = list(input())
+
+if list(reversed(word)) == word:
+    print(1)
+else:
+    print(0)
