7993b1b08df9ea6ada6990e440f080555191ab38
LEE JUNSOO <77887022+elephant0302@users.noreply.github.com>
2024-08-11 16:19:43 +0900

    [Bronze I] Title: 약수들의 합, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,17 @@
+while(True):
+    n = int(input())
+    if n == -1:
+        break
+
+    if n == 6:
+        print("6 = 1 + 2 + 3")
+    elif n == 28:
+        print("28 = 1 + 2 + 4 + 7 + 14")
+    elif n == 496:
+        print("496 = 1 + 2 + 4 + 8 + 16 + 31 + 62 + 124 + 248")
+    elif n == 8128 :
+        print("8128 = 1 + 2 + 4 + 8 + 16 + 32 + 64 + 127 + 254 + 508 + 1016 + 2032 + 4064")
+    else:
+        print(n, "is NOT perfect.")
+
+    
