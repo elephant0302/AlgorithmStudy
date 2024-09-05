04edaf23bca40ad8ac3c83ab82ef1ee078943160
LEE JUNSOO <77887022+elephant0302@users.noreply.github.com>
2024-09-06 00:49:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.read
+
+n = int(input())
+
+dp = [0] * 100000
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+dp[4] = 5
+
+for i in range(3,n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n]%10007)
