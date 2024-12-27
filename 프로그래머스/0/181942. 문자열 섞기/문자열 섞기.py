bd00883c2350f659915f22a4ca0e317c83768513
LEE JUNSOO <77887022+elephant0302@users.noreply.github.com>
2024-12-27 19:41:54 +0900

    [level 0] Title: 문자열 섞기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,10 +1,7 @@
 def solution(str1, str2):
     answer = ''
-    for i in range(len(str1) + len(str2)):
-        if i % 2 == 0 :
-            answer += str1[int(i/2)]
-        else:
-            answer += str2[int(i/2)]
+    for i in range(0,len(str1)):
+        answer += str1[i] + str2[i]
             
     
     return answer
