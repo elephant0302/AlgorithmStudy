33d60e3faa6a01ca87e172939a54b9abbe9de11f
LEE JUNSOO <77887022+elephant0302@users.noreply.github.com>
2024-12-28 16:45:47 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -0,0 +1,8 @@
+def solution(arr, queries):
+    answer = []
+    
+    for i in range(len(queries)):
+        arr[queries[i][0]], arr[queries[i][1]] =  arr[queries[i][1]],  arr[queries[i][0]]
+        
+    answer = arr
+    return answer
